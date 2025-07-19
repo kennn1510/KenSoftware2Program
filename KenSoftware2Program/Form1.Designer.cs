45b7b09ca77e243afe8ecf758b492bb2024444f9
KenVS2022 <46609017+kennn1510@users.noreply.github.com>
2025-07-19 14:57:33 -0500

    Add login label

--- KenSoftware2Program/Form1.Designer.cs
+++ KenSoftware2Program/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.ConnectB = new System.Windows.Forms.Button();
+            this.LoginLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ConnectB
@@ -41,21 +42,34 @@
             this.ConnectB.UseVisualStyleBackColor = true;
             this.ConnectB.Click += new System.EventHandler(this.button1_Click);
             // 
+            // LoginLabel
+            // 
+            this.LoginLabel.AutoSize = true;
+            this.LoginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginLabel.Location = new System.Drawing.Point(323, 9);
+            this.LoginLabel.Name = "LoginLabel";
+            this.LoginLabel.Size = new System.Drawing.Size(85, 32);
+            this.LoginLabel.TabIndex = 1;
+            this.LoginLabel.Text = "Login";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LoginLabel);
             this.Controls.Add(this.ConnectB);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button ConnectB;
+        private System.Windows.Forms.Label LoginLabel;
     }
 }
 
