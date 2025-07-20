141e421f2182d3627e4bf6f643f75d43f4262998
KenVS2022 <46609017+kennn1510@users.noreply.github.com>
2025-07-19 21:02:52 -0500

    Got GeoCoordinateWatcher to work in finding location, now have to made it pretty next

--- KenSoftware2Program/Forms/LoginForm.cs
+++ KenSoftware2Program/Forms/LoginForm.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Device.Location;
 using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
-using System.Device.Location;
+using static Google.Protobuf.Reflection.SourceCodeInfo.Types;
 
 
 namespace KenSoftware2Program
@@ -27,17 +28,51 @@ namespace KenSoftware2Program
         {
             CultureInfo culture = CultureInfo.CurrentCulture;
 
+            CLocation myLocation = new CLocation();
+            myLocation.GetLocationEvent();
+            Console.WriteLine("Enter any key to quit.");
+            Console.ReadLine();
+
+
+
             if (culture.TwoLetterISOLanguageName == "en")
             {
                 Console.WriteLine("Translated to english");
+                //LocationLabel.Text = $"Location: {latitude}, {longitude}";
             } else
             {
                 Console.WriteLine("Traduit en français");
                 this.Text = "Formulaire de connexion";
-                LocationLabel.Text = "Emplacement";
+                LocationLabel.Text = $"Emplacement: ";
                 UsernameLabel.Text = "Nom d'utilisateur:";
                 PasswordLabel.Text = "Mot de passe:";
                 LoginButton.Text = "Se connecter";
+            }
+        }
+
+        class CLocation
+        {
+            GeoCoordinateWatcher watcher;
+
+            public void GetLocationEvent()
+            {
+                this.watcher = new GeoCoordinateWatcher();
+                this.watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+                bool started = this.watcher.TryStart(false, TimeSpan.FromMilliseconds(2000));
+                if (!started)
+                {
+                    Console.WriteLine("GeoCoordinateWatcher timed out on start.");
+                }
+            }
+
+            void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+            {
+                PrintPosition(e.Position.Location.Latitude, e.Position.Location.Longitude);
+            }
+
+            void PrintPosition(double Latitude, double Longitude)
+            {
+                Console.WriteLine("Latitude: {0}, Longitude {1}", Latitude, Longitude);
             }
         }
 
