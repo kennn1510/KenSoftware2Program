3a0715fc865ed2c63cc3249574640a1343f8c983
KenVS2022 <46609017+kennn1510@users.noreply.github.com>
2025-07-24 17:10:38 -0500

    Add CRUD buttons

--- KenSoftware2Program/Forms/CustomerForm.Designer.cs
+++ KenSoftware2Program/Forms/CustomerForm.Designer.cs
@@ -31,6 +31,10 @@
             this.RefreshButton = new System.Windows.Forms.Button();
             this.LogoutButton = new System.Windows.Forms.Button();
             this.CustomerDataGridView = new System.Windows.Forms.DataGridView();
+            this.AddCustomerButton = new System.Windows.Forms.Button();
+            this.UpdateCustomerButton = new System.Windows.Forms.Button();
+            this.DeleteCustomerButton = new System.Windows.Forms.Button();
+            this.ManageAppointmentsButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.CustomerDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -62,11 +66,51 @@
             this.CustomerDataGridView.Size = new System.Drawing.Size(776, 367);
             this.CustomerDataGridView.TabIndex = 2;
             // 
+            // AddCustomerButton
+            // 
+            this.AddCustomerButton.Location = new System.Drawing.Point(12, 415);
+            this.AddCustomerButton.Name = "AddCustomerButton";
+            this.AddCustomerButton.Size = new System.Drawing.Size(150, 23);
+            this.AddCustomerButton.TabIndex = 3;
+            this.AddCustomerButton.Text = "Add Customer";
+            this.AddCustomerButton.UseVisualStyleBackColor = true;
+            // 
+            // UpdateCustomerButton
+            // 
+            this.UpdateCustomerButton.Location = new System.Drawing.Point(205, 415);
+            this.UpdateCustomerButton.Name = "UpdateCustomerButton";
+            this.UpdateCustomerButton.Size = new System.Drawing.Size(150, 23);
+            this.UpdateCustomerButton.TabIndex = 4;
+            this.UpdateCustomerButton.Text = "Update Customer";
+            this.UpdateCustomerButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteCustomerButton
+            // 
+            this.DeleteCustomerButton.Location = new System.Drawing.Point(393, 414);
+            this.DeleteCustomerButton.Name = "DeleteCustomerButton";
+            this.DeleteCustomerButton.Size = new System.Drawing.Size(150, 23);
+            this.DeleteCustomerButton.TabIndex = 5;
+            this.DeleteCustomerButton.Text = "Delete Customer";
+            this.DeleteCustomerButton.UseVisualStyleBackColor = true;
+            // 
+            // ManageAppointmentsButton
+            // 
+            this.ManageAppointmentsButton.Location = new System.Drawing.Point(597, 414);
+            this.ManageAppointmentsButton.Name = "ManageAppointmentsButton";
+            this.ManageAppointmentsButton.Size = new System.Drawing.Size(191, 23);
+            this.ManageAppointmentsButton.TabIndex = 6;
+            this.ManageAppointmentsButton.Text = "Manage Appointments";
+            this.ManageAppointmentsButton.UseVisualStyleBackColor = true;
+            // 
             // CustomerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ManageAppointmentsButton);
+            this.Controls.Add(this.DeleteCustomerButton);
+            this.Controls.Add(this.UpdateCustomerButton);
+            this.Controls.Add(this.AddCustomerButton);
             this.Controls.Add(this.CustomerDataGridView);
             this.Controls.Add(this.LogoutButton);
             this.Controls.Add(this.RefreshButton);
@@ -82,5 +126,9 @@
         private System.Windows.Forms.Button RefreshButton;
         private System.Windows.Forms.Button LogoutButton;
         private System.Windows.Forms.DataGridView CustomerDataGridView;
+        private System.Windows.Forms.Button AddCustomerButton;
+        private System.Windows.Forms.Button UpdateCustomerButton;
+        private System.Windows.Forms.Button DeleteCustomerButton;
+        private System.Windows.Forms.Button ManageAppointmentsButton;
     }
 }
