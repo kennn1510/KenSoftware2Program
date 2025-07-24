3edcd5c85d88a4ac86c84e7dbbfd5cc5c01a395a
KenVS2022 <46609017+kennn1510@users.noreply.github.com>
2025-07-24 17:04:12 -0500

    Add SetUpForm for CustomerDataGridView, query name, address, phone

--- KenSoftware2Program/Forms/CustomerForm.Designer.cs
+++ KenSoftware2Program/Forms/CustomerForm.Designer.cs
@@ -28,19 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.LogoutButton = new System.Windows.Forms.Button();
+            this.CustomerDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomerDataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Location = new System.Drawing.Point(12, 12);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(75, 23);
+            this.RefreshButton.TabIndex = 0;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            // 
+            // LogoutButton
+            // 
+            this.LogoutButton.Location = new System.Drawing.Point(713, 12);
+            this.LogoutButton.Name = "LogoutButton";
+            this.LogoutButton.Size = new System.Drawing.Size(75, 23);
+            this.LogoutButton.TabIndex = 1;
+            this.LogoutButton.Text = "Logout";
+            this.LogoutButton.UseVisualStyleBackColor = true;
+            // 
+            // CustomerDataGridView
+            // 
+            this.CustomerDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomerDataGridView.Location = new System.Drawing.Point(12, 41);
+            this.CustomerDataGridView.Name = "CustomerDataGridView";
+            this.CustomerDataGridView.RowHeadersWidth = 51;
+            this.CustomerDataGridView.RowTemplate.Height = 24;
+            this.CustomerDataGridView.Size = new System.Drawing.Size(776, 367);
+            this.CustomerDataGridView.TabIndex = 2;
             // 
             // CustomerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CustomerDataGridView);
+            this.Controls.Add(this.LogoutButton);
+            this.Controls.Add(this.RefreshButton);
             this.Name = "CustomerForm";
             this.Text = "Customer Form";
+            ((System.ComponentModel.ISupportInitialize)(this.CustomerDataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.Button LogoutButton;
+        private System.Windows.Forms.DataGridView CustomerDataGridView;
     }
 }
--- KenSoftware2Program/Forms/CustomerForm.cs
+++ KenSoftware2Program/Forms/CustomerForm.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KenSoftware2Program.Forms
@@ -15,6 +10,34 @@ namespace KenSoftware2Program.Forms
         public CustomerForm()
         {
             InitializeComponent();
+            SetUpForm();
+        }
+
+        private void SetUpForm()
+        {
+            try
+            {
+                using (Database.DBConnection.conn)
+                {
+                    string query = @"
+                        SELECT c.customerName, a.address, a.phone
+                        FROM customer c
+                        LEFT JOIN address a ON c.addressId = a.addressId";
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(query, Database.DBConnection.conn);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    CustomerDataGridView.DataSource = dataTable;
+                    CustomerDataGridView.Columns["customerName"].HeaderText = "Customer Name";
+                    CustomerDataGridView.Columns["address"].HeaderText = "Address";
+                    CustomerDataGridView.Columns["phone"].HeaderText = "Phone Number";
+
+                    CustomerDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
     }
 }
