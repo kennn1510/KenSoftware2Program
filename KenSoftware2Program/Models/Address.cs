550166b05e60fb3c5ad248534097b4138bd139b5
KenVS2022 <46609017+kennn1510@users.noreply.github.com>
2025-07-24 19:32:28 -0500

    Add Address model with auto-implemented properties

---
+++ KenSoftware2Program/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KenSoftware2Program.Models
+{
+    internal class Address
+    {
+        public int AddressId { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public int CityId { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string LastUpdateBy { get; set; }
+    }
+}
