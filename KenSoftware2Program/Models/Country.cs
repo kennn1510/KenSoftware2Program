1174c5abf4e81cce9d3a36c943c39fcdfa89df54
KenVS2022 <46609017+kennn1510@users.noreply.github.com>
2025-07-24 19:36:41 -0500

    Add Country model with auto-implemented properties

---
+++ KenSoftware2Program/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KenSoftware2Program.Models
+{
+    internal class Country
+    {
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string LastUpdateBy { get; set; }
+    }
+}
