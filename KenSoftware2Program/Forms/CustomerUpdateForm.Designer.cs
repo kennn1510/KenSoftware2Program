a8111369b8dc7ca46f8134e0e86f0679a256c93c
KenVS2022 <46609017+kennn1510@users.noreply.github.com>
2025-07-30 22:34:59 -0500

    Add CustomerUpdateForm with new UI elements and resources
    Updated `CustomerUpdateForm.Designer.cs` to include new text boxes and labels for country, postal code, city, address lines, phone number, and name. Added a submit button and changed the form title to "Customer Update Form".
    
    Modified `KenSoftware2Program.csproj` to include an embedded resource for `CustomerUpdateForm.resx`, enabling localization.
    
    Added `CustomerUpdateForm.resx` for resource management, containing the necessary XML structure for handling localized strings and other resources.

--- KenSoftware2Program/Forms/CustomerUpdateForm.Designer.cs
+++ KenSoftware2Program/Forms/CustomerUpdateForm.Designer.cs
@@ -28,12 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CountryTextBox = new System.Windows.Forms.TextBox();
+            this.CountryLabel = new System.Windows.Forms.Label();
+            this.PostalCodeTextBox = new System.Windows.Forms.TextBox();
+            this.PostalCodeLabel = new System.Windows.Forms.Label();
+            this.CityTextBox = new System.Windows.Forms.TextBox();
+            this.CityLabel = new System.Windows.Forms.Label();
+            this.Address2TextBox = new System.Windows.Forms.TextBox();
+            this.Address2Label = new System.Windows.Forms.Label();
+            this.Address1TextBox = new System.Windows.Forms.TextBox();
+            this.Address1Label = new System.Windows.Forms.Label();
+            this.PhoneNumberLabel = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CountryTextBox
+            // 
+            this.CountryTextBox.Location = new System.Drawing.Point(350, 315);
+            this.CountryTextBox.Name = "CountryTextBox";
+            this.CountryTextBox.Size = new System.Drawing.Size(212, 22);
+            this.CountryTextBox.TabIndex = 31;
+            // 
+            // CountryLabel
+            // 
+            this.CountryLabel.AutoSize = true;
+            this.CountryLabel.Location = new System.Drawing.Point(289, 315);
+            this.CountryLabel.Name = "CountryLabel";
+            this.CountryLabel.Size = new System.Drawing.Size(55, 16);
+            this.CountryLabel.TabIndex = 30;
+            this.CountryLabel.Text = "Country:";
+            // 
+            // PostalCodeTextBox
+            // 
+            this.PostalCodeTextBox.Location = new System.Drawing.Point(350, 268);
+            this.PostalCodeTextBox.Name = "PostalCodeTextBox";
+            this.PostalCodeTextBox.Size = new System.Drawing.Size(212, 22);
+            this.PostalCodeTextBox.TabIndex = 29;
+            // 
+            // PostalCodeLabel
+            // 
+            this.PostalCodeLabel.AutoSize = true;
+            this.PostalCodeLabel.Location = new System.Drawing.Point(255, 271);
+            this.PostalCodeLabel.Name = "PostalCodeLabel";
+            this.PostalCodeLabel.Size = new System.Drawing.Size(84, 16);
+            this.PostalCodeLabel.TabIndex = 28;
+            this.PostalCodeLabel.Text = "Postal Code:";
+            // 
+            // CityTextBox
+            // 
+            this.CityTextBox.Location = new System.Drawing.Point(350, 217);
+            this.CityTextBox.Name = "CityTextBox";
+            this.CityTextBox.Size = new System.Drawing.Size(212, 22);
+            this.CityTextBox.TabIndex = 27;
+            // 
+            // CityLabel
+            // 
+            this.CityLabel.AutoSize = true;
+            this.CityLabel.Location = new System.Drawing.Point(307, 217);
+            this.CityLabel.Name = "CityLabel";
+            this.CityLabel.Size = new System.Drawing.Size(32, 16);
+            this.CityLabel.TabIndex = 26;
+            this.CityLabel.Text = "City:";
+            // 
+            // Address2TextBox
+            // 
+            this.Address2TextBox.Location = new System.Drawing.Point(350, 172);
+            this.Address2TextBox.Name = "Address2TextBox";
+            this.Address2TextBox.Size = new System.Drawing.Size(212, 22);
+            this.Address2TextBox.TabIndex = 25;
+            // 
+            // Address2Label
+            // 
+            this.Address2Label.AutoSize = true;
+            this.Address2Label.Location = new System.Drawing.Point(268, 175);
+            this.Address2Label.Name = "Address2Label";
+            this.Address2Label.Size = new System.Drawing.Size(71, 16);
+            this.Address2Label.TabIndex = 24;
+            this.Address2Label.Text = "Address 2:";
+            // 
+            // Address1TextBox
+            // 
+            this.Address1TextBox.Location = new System.Drawing.Point(350, 134);
+            this.Address1TextBox.Name = "Address1TextBox";
+            this.Address1TextBox.Size = new System.Drawing.Size(212, 22);
+            this.Address1TextBox.TabIndex = 23;
+            // 
+            // Address1Label
+            // 
+            this.Address1Label.AutoSize = true;
+            this.Address1Label.Location = new System.Drawing.Point(268, 140);
+            this.Address1Label.Name = "Address1Label";
+            this.Address1Label.Size = new System.Drawing.Size(71, 16);
+            this.Address1Label.TabIndex = 22;
+            this.Address1Label.Text = "Address 1:";
+            // 
+            // PhoneNumberLabel
+            // 
+            this.PhoneNumberLabel.AutoSize = true;
+            this.PhoneNumberLabel.Location = new System.Drawing.Point(239, 99);
+            this.PhoneNumberLabel.Name = "PhoneNumberLabel";
+            this.PhoneNumberLabel.Size = new System.Drawing.Size(100, 16);
+            this.PhoneNumberLabel.TabIndex = 21;
+            this.PhoneNumberLabel.Text = "Phone Number:";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(289, 49);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(47, 16);
+            this.NameLabel.TabIndex = 20;
+            this.NameLabel.Text = "Name:";
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(350, 96);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(212, 22);
+            this.PhoneNumberTextBox.TabIndex = 19;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(350, 46);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(212, 22);
+            this.NameTextBox.TabIndex = 18;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(292, 361);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(270, 43);
+            this.SubmitButton.TabIndex = 17;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            // 
+            // CustomerUpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CustomerUpdateForm";
+            this.Controls.Add(this.CountryTextBox);
+            this.Controls.Add(this.CountryLabel);
+            this.Controls.Add(this.PostalCodeTextBox);
+            this.Controls.Add(this.PostalCodeLabel);
+            this.Controls.Add(this.CityTextBox);
+            this.Controls.Add(this.CityLabel);
+            this.Controls.Add(this.Address2TextBox);
+            this.Controls.Add(this.Address2Label);
+            this.Controls.Add(this.Address1TextBox);
+            this.Controls.Add(this.Address1Label);
+            this.Controls.Add(this.PhoneNumberLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.PhoneNumberTextBox);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.SubmitButton);
+            this.Name = "CustomerUpdateForm";
+            this.Text = "Customer Update Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox CountryTextBox;
+        private System.Windows.Forms.Label CountryLabel;
+        private System.Windows.Forms.TextBox PostalCodeTextBox;
+        private System.Windows.Forms.Label PostalCodeLabel;
+        private System.Windows.Forms.TextBox CityTextBox;
+        private System.Windows.Forms.Label CityLabel;
+        private System.Windows.Forms.TextBox Address2TextBox;
+        private System.Windows.Forms.Label Address2Label;
+        private System.Windows.Forms.TextBox Address1TextBox;
+        private System.Windows.Forms.Label Address1Label;
+        private System.Windows.Forms.Label PhoneNumberLabel;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox PhoneNumberTextBox;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Button SubmitButton;
     }
 }
