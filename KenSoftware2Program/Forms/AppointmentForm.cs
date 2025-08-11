26dfc364b3329124309a6aa1bdd6a68a72fcdb28
KenVS2022 <46609017+kennn1510@users.noreply.github.com>
2025-08-11 09:33:36 +0700

    Add query for customer name to appointments

--- KenSoftware2Program/Forms/AppointmentForm.cs
+++ KenSoftware2Program/Forms/AppointmentForm.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
 
 namespace KenSoftware2Program.Forms
 {
@@ -10,7 +13,47 @@ namespace KenSoftware2Program.Forms
         {
             InitializeComponent();
             this.customerId = customerId;
+            SetUpForm();
+        }
 
+        private void SetUpForm()
+        {
+            try
+            {
+                if (Database.DBConnection.conn.State != ConnectionState.Open)
+                    Database.DBConnection.conn.Open();
+                using (MySqlCommand command = new MySqlCommand())
+                {
+                    command.Connection = Database.DBConnection.conn;
+                    command.CommandText = @"
+                        SELECT customerName
+                        FROM customer
+                        WHERE customerId = @CustomerId";
+                    command.Parameters.AddWithValue("@CustomerId", customerId);
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            CustomerNameLabel.Text = "Customer Name: " + reader["customerName"].ToString();
+                        }
+                        else
+                        {
+                            throw new Exception("Customer not found.");
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading customer data: " + ex.Message);
+            }
+            finally
+            {
+                if (Database.DBConnection.conn.State == ConnectionState.Open)
+                    Database.DBConnection.conn.Close();
+            }
         }
     }
 }
